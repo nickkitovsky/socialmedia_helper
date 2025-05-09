ad8d6d2c4cdbcf3472a7e168ad7e0aaf2cc5a5c4
Nick <1825545+nickkitovsky@users.noreply.github.com>
2025-05-10 02:33:11 +0700

    Add logging message

--- src/link_detector.py
+++ src/link_detector.py
@@ -11,7 +11,8 @@ def is_tiktok_url(text: str) -> bool:
             if parsed.scheme in {"http", "https"} and "tiktok.com" in parsed.netloc:
                 logger.debug("Recieved tiktok url: %s", text)
                 return True
-    except Exception:  # noqa: BLE001
+    except Exception as e:  # noqa: BLE001
+        logger.warning("Failure parse message: %s, cause: %s", text, e)
         return False
     else:
         return False
