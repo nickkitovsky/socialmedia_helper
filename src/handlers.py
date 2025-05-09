3324d76783247fce943064324267ce013aa922ad
Nick <1825545+nickkitovsky@users.noreply.github.com>
2025-05-10 01:35:19 +0700

    add delete_file_later function

--- src/handlers.py
+++ src/handlers.py
@@ -1,3 +1,4 @@
+import asyncio
 import pathlib
 from asyncio import sleep as asleep
 
@@ -12,16 +13,25 @@ WAIT_FOR_DELETE_FILE_SEC = 5 * 60
 class GroupMessageHandler:
     def __init__(self, bot: Bot):
         self.bot = bot
-        self.tiktok_downloader = TikTokDownloader()
+        self.tiktok = TikTokDownloader()
 
     async def handle_group_message(self, message: Message) -> None:
         if isinstance(message.text, str) and is_tiktok_url(message.text):
-            file_path = await self.tiktok_downloader.download(message.text)
+            file_path = await self.tiktok.download(message.text)
             if file_path:
                 await self.bot.send_video(
                     chat_id=message.chat.id,
                     video=FSInputFile(file_path),
                 )
                 await message.delete()
-                await asleep(WAIT_FOR_DELETE_FILE_SEC)
-                pathlib.Path(file_path).unlink()
+
+                asyncio.create_task(self._delete_file_later(file_path))  # noqa: RUF006
+
+    async def _delete_file_later(self, file_path: str) -> None:
+        await asleep(WAIT_FOR_DELETE_FILE_SEC)
+        try:
+            pathlib.Path(file_path).unlink()
+        except Exception:  # noqa: BLE001
+            pass
+            # Можно логировать или игнорировать
+            # print(f"Ошибка при удалении файла {file_path}: {e}")
